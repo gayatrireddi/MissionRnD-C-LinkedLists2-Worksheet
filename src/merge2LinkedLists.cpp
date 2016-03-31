7b78e5ce7b31d71245ef823879b71e8a653cb9c9
gayatrireddi <gayatrireddi@missionrnd.com>
2016-03-31 21:53:22 +0530

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -10,14 +10,69 @@ ERROR CASES: Return NULL for error cases.
 
 NOTES:
 */
-
+#include<malloc.h>
 #include <stdio.h>
 
 struct node {
 	int num;
 	struct node *next;
 };
-
+struct node *insert(struct node *first, int x);
 struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
-	return NULL;
+	if (head1 == NULL)
+		return head2;
+	if (head2 == NULL)
+		return head1;
+	if (head1 == NULL&&head2 == NULL)
+		return NULL;
+	node *p, *c, *k, *head;
+	k = NULL;
+	p = head1;
+	c = head2;
+	
+	while (p != NULL&&c != NULL)
+	{
+		if (p->num < c->num)
+		{
+			k = insert(k, p->num);
+			p = p->next;
+		}
+		else{
+			k = insert(k, c->num);
+			c = c->next;
+		}
+	}
+	while (c != NULL&&p==NULL)
+	{
+		k = insert(k, c->num);
+		c = c->next;
+	}
+	while (p != NULL&&c == NULL)
+	{
+		k = insert(k, p->num);
+		p = p->next;
+	}
+	return k;
 }
+struct node *insert(struct node *first, int x)
+{
+	struct node *t, *q;
+	q = (struct node*)malloc(sizeof(node));
+	q->num = x;
+	q->next = NULL;
+	if (first == NULL)
+	{
+		first = q;
+		return first;
+	}
+	t = first;
+	while (t->next != NULL)
+	{
+		t = t->next;
+	}
+	t->next = q;
+	return first;
+}
+
+
+	
